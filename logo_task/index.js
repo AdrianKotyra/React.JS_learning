900106a153c3c8cbdb8016f286274a9ed609412c
Adrian <83014095+AdrianKotyra@users.noreply.github.com>
2022-07-18 10:30:55 +0100

    Add files via upload

---
+++ logo_task/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import "./Appcss.css";
+
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById("top_bar"))
+
+
+
