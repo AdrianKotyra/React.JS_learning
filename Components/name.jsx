3add0a5c169696dd3c9d01323c999b98da191690
Adrian <83014095+AdrianKotyra@users.noreply.github.com>
2022-07-19 10:40:21 +0100

    Add files via upload
    Bussiness Card Task

---
+++ Components/name.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+function Name () {
+    return(
+        <div id="name_container">
+             <h1>Adrian Kotyra</h1>
+            <p1>Fronted-developer</p1>
+        </div>
+       
+
+    )
+}
+
+export default Name
