8eb1c03dd65fef9897eb5d8757e8cf5109760f32
Adrian <83014095+AdrianKotyra@users.noreply.github.com>
2022-07-19 10:41:41 +0100

    Add files via upload

---
+++ bussiness card_task/about.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About_section() {
+    return (
+        <div id="about_section">
+
+            <h1>About</h1>
+            <p1>Hello, My name is Adrian. I am a student of Web Design and software development at Edinburgh College. 
+                I love programming, weightlifting, and self-development. In the future I see myself working as a software developer
+            </p1>
+        </div>
+
+
+
+
+
+    )
+}
+
+export default About_section;
